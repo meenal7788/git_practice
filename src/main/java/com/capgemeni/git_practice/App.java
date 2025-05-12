147b330739c458d514cc61d0c96622f17983988a
meenal7788 <meenalnale2002@gmail.com>
2025-05-12 12:01:26 +0530

    first commit

---
+++ src/main/java/com/capgemeni/git_practice/App.java
@@ -0,0 +1,13 @@
+package com.capgemeni.git_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/capgemeni/git_practice/Hello.java
@@ -0,0 +1,7 @@
+package com.capgemeni.git_practice;
+
+public class Hello {
+ public static void main(String[] args) {
+	System.out.println("Hello world");
+}
+}
